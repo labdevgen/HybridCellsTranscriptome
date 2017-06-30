7296e7f68359adf8f7c021b6b7f09ce384005299
labdevgen <zakazserov@gmail.com>
2017-06-30 15:36:40 +0700

    New clink script
    In fact, I didn't run clink, proceed immediatelly to cdif

--- scripts/clink.py
+++ scripts/clink.py
@@ -10,20 +10,26 @@ def executeBashCommand(bashCommand,doPrint=True):
   else:
     return p.communicate()[0]
 
-work_dir = "/mnt/storage/home/vsfishman/tmp/m5s/"
-tophat_out_dir="/mnt/storage/home/vsfishman/tmp/m5s/tophat_out/"
-gff_file="/mnt/storage/home/vsfishman/tmp/m5s/transcriptome_data/mm10known.gff"
-fasta_file="/mnt/storage/home/vsfishman/tmp/m5s/Bowtie2Index/genome.fa"
+work_dir = "/mnt/storage/home/vsfishman/tmp/m5s_2017/"
+tophat_out_dir="/mnt/storage/home/vsfishman/tmp/m5s_2017/tophat_out/"
+gff_file="/mnt/storage/home/vsfishman/tmp/m5s_2017/transcriptome_data/mm10known.gff"
+fasta_file="/mnt/storage/home/vsfishman/HiC/bin/bowtie2/index/mm10.fa"
 
-file_names=range(1,2)
-for i,v in enumerate(file_names):
-		file_names[i]="%(v)03d"%{"v":v}
+allfiles=os.listdir(tophat_out_dir)
 
-
-for i in file_names:
+for i in allfiles:
 	f=open("clink.q","w")
-	f.write("#!/bin/bash \n#PBS -V -r n \n#PBS -l select=1:ncpus=4:mem=60gb,cput=64:10:00,walltime=32:15:00\n#PBS -q vkop2q\n#PBS -N clink"+i+"\n#PBS -j oe\ndate\n#!/bin/sh\ndate\n")
+	f.write("""#!/bin/bash
+#PBS -V -r n 
+#PBS -l select=1:ncpus=12:mem=24gb,cput=164:10:00,walltime=32:15:00
+#PBS -q bl2x220g7q
+#PBS -N clink"""+i+"""
+#PBS -j oe
+date"""+
+"\nexport PATH=$HOME/Distr/bowtie2-2.2.1:$PATH\n"+ #change bowtie2 version to old 2.2.1
+"export PATH=$HOME/Distr/samtools-0.1.19/:$PATH\n" #change samtools version to old 0.1.19
+)
 	f.write("cd "+work_dir+"\n")
-	f.write("cufflinks -b "+fasta_file+" -G "+gff_file+" --multi-read-correct -p 4 -o clinks_out/"+i+" --no-update-check "+tophat_out_dir+i+"_trimmed/accepted_hits.bam\n")
+	f.write("cufflinks -b "+fasta_file+" -G "+gff_file+" --multi-read-correct -p 12 -o clinks_out/"+i+" --no-update-check "+tophat_out_dir+i+"/accepted_hits.bam\n")
 	f.close()
-	executeBashCommand("qsub clink.q")
+#	executeBashCommand("qsub clink.q")
